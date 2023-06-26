1cfe66d94ee30e3c03f27add3d023a7c4a2fb75b
m-h0pe <133053207+m-h0pe@users.noreply.github.com>
2023-06-26 09:18:27 +0000

    last

---
+++ GENIN/WEEK ONE/BaNdname.py
@@ -0,0 +1,5 @@
+# Creating your Band Name
+print("Welcome to the Band name Generator")
+Hname= input("What's the name of the City yoj grew up in?\n")
+Pet_name= input("What is your pet name?\n")
+print(f"Your band name could be+" "+{Hname}+{Pet_name}")
